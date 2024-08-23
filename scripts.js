b07cb90be2fa0ee596ecc7d0af234642e5e4196e
polaveh <167043541+polaveh@users.noreply.github.com>
2024-08-23 01:05:57 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const btn = document.querySelector('.btn-primary');
+    if (btn) {
+        btn.addEventListener('click', function() {
+            alert('¡Gracias por revisar mis proyectos!');
+        });
+    }
+});
